dc3cc18aea65a2019058410c091240ac2765a500
RSLastra <raul.slastra@gmail.com>
2024-11-17 17:07:10 -0600

    feat: task model

---
+++ models/Task.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const TaskSchema = Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pendiente', 'en curso', 'completada'],
+        default: 'pendiente'
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }
+
+});
+
+module.exports = model('Task', TaskSchema); 
