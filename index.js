cb15a224f6f9b9179bd00d72fa85aaf076f7930a
RSLastra <raul.slastra@gmail.com>
2024-11-17 17:17:31 -0600

    feat: auth router

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const {dbConnection} = require('./database/config');
+const { dbConnection } = require('./database/config');
+const authRouter  = require('./routes/auth');
 require('dotenv').config();
 
 const { PORT } = process.env;
 
 const app = express();
 
+// Conexion a base de datos
 dbConnection();
+
+//Rutas
+app.use('/api/auth', authRouter);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
