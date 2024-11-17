13c6aa752eda0b3b2ebe170499013a62cf1973af
RSLastra <raul.slastra@gmail.com>
2024-11-17 16:54:16 -0600

    feat: database config

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const {dbConnection} = require('./database/config');
+require('dotenv').config();
+
+const { PORT } = process.env;
 
 const app = express();
 
+dbConnection();
 
-app.listen(3001, () => {
-  console.log(`Servidor corriendo en el puerto 3001`);
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
 })
