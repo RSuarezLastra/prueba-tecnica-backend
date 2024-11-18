1a74319a3198b2a7f762ee7a6d54a130f695e122
RSLastra <raul.slastra@gmail.com>
2024-11-18 11:44:08 -0600

    change server variable name

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 // Conexion a base de datos
 dbConnection();
 
-server.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
