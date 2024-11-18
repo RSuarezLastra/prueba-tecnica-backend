819fc2b19216d3f257ec89a7533412576f7f7790
RSLastra <raul.slastra@gmail.com>
2024-11-18 12:00:04 -0600

    cors change

--- index.js
+++ index.js
@@ -13,13 +13,7 @@ const app = express();
 // Conexion a base de datos
 dbConnection();
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+app.use(cors());
 
 app.use(express.json());
 
